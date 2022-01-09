e7a133ec1f5996ddadeea712480b9096961dc0a0
myselfparag <mailing2parag@gmail.com>
2022-01-09 17:42:53 +0530

    Renames nfp1 to nfp_1_additional

---
+++ nfp_1_additional.py
@@ -0,0 +1,36 @@
+#Additonal information regarding page 33-34
+# Frozensets
+# initialize A and B
+A = frozenset([1, 2, 3, 4])
+B = frozenset([3, 4, 5, 6])
+
+# copying a frozenset
+C = A.copy()  # Output: frozenset({1, 2, 3, 4})
+print(C)
+
+# union
+print(A.union(B))  # Output: frozenset({1, 2, 3, 4, 5, 6})
+
+# intersection
+print(A.intersection(B))  # Output: frozenset({3, 4})
+
+# difference
+print(A.difference(B))  # Output: frozenset({1, 2})
+
+# symmetric_difference
+print(A.symmetric_difference(B))  # Output: frozenset({1, 2, 5, 6})
+
+# Frozensets
+# initialize A, B and C
+A = frozenset([1, 2, 3, 4])
+B = frozenset([3, 4, 5, 6])
+C = frozenset([5, 6])
+
+# isdisjoint() method
+print(A.isdisjoint(C))  # Output: True
+
+# issubset() method
+print(C.issubset(B))  # Output: True
+
+# issuperset() method
+print(B.issuperset(C))  # Output: True
