08abd6bb1a0abc781931d64dd4f40daca42066ae
myselfparag <mailing2parag@gmail.com>
2022-01-12 14:38:48 +0530

    functions

---
+++ functions/basics_of_function_prog.py
@@ -0,0 +1,6 @@
+#Write a program to create a function that takes two arguments, name and age, and print their value.
+
+
+def print_fn(name,age):
+    print(name,age)
+print_fn("ch",9)
--- functions/mix_basic_optional_hw.py
+++ functions/mix_basic_optional_hw.py
@@ -9,13 +9,34 @@ products_with_price = {"Deo":200,"Shampoo":220,"Powder":150,"Soap":30,"Specs":10
 
 COUPON_LIST = {"happyny22" : 10, "ny22flat" : 12, "spiderman22hm" : 13, "talent22" : 12}
 
-## Expected Input : List of items
-## Expected Output : Total bill after discount
-product_name = input("Enter the name of product")
-Coupon= input("ENTER THE COUPON FROM THE MENTIONED LIST")
+# product_lis = input("Give the list of your product, please use space after each name")
+# product_lis = product_lis.split(" ")
 
-def bill_calculator(product_name,Coupon):
-    total_bill = 0
-    for item in product_name:
-        print(232)
+def tb_calculator(product):
+    if product in products_with_price.keys():
+        return products_with_price[product]
+
+def coupon_applier(total_bill, coupon_codes):
+    max_value = -1
+    for cc in coupon_codes:
+        if max_value < COUPON_LIST[cc]:
+            print(f"Replacing {max_value} by {COUPON_LIST[cc]}, using_coupon_name : {cc}")
+            max_value = COUPON_LIST[cc]
+            
+        
+    return total_bill - (total_bill * max_value * 0.01)
+    
+product_list = ["Deo","Shampoo","Powder"]
+coupon_codes = ["ny22flat", "spiderman22hm", "talent22"]
+total_bill = 0
+for pl in product_list:
+    print(pl)
+    product_cost = tb_calculator(pl)
+    total_bill += product_cost
+
+final_bill = coupon_applier(total_bill, coupon_codes)
+print(f"Total Bill : {total_bill}, Bill after coupon {coupon_codes} : {final_bill}")
+
+
+    
         
